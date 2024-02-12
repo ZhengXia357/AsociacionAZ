5896746ae721d0ad5e11f233319b8cff140be756
pierocisneros <pdcb02@educa.madrid.org>
2024-02-12 19:29:26 +0100

    commit de registro casi 90% terminado

---
+++ js/registrer.js
@@ -0,0 +1,14 @@
+
+var pop = document.getElementById('popUp')
+console.log(pop);
+
+
+function mostrarPopUp(){
+
+    pop.classList.add("mostrar-pop");
+
+
+}
+function cerrarPopUp(){
+    pop.classList.remove("mostrar-pop");
+}
