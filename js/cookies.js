c49ac884414ff5598523c04952a63528cd769c76
RubenCantal <rpc155@educa.madrid.org>
2024-02-18 19:40:32 +0100

    correctiones

--- js/cookies.js
+++ js/cookies.js
@@ -8,7 +8,9 @@ function mostrarVentanaCookies() {
     const ventanaEmergente = document.createElement("div");
     ventanaEmergente.innerHTML = `
         <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: #f0f0f0; padding: 20px; text-align: center;">
-            <table  style="width: 100%">
+        <div>   
+        <table  style="width: 100%">
+            
             <tr>
                 <td>
                 <h2><font face="Verdana, Geneva, Tahoma, sans-serif"> En StellarUs utilizamos cookies para mejorar la experiencia del usuario. Al continuar navegando por este sitio, aceptas nuestro uso de cookies. <a href="../html/coockie.html">Leer más</a></font></h2>
@@ -17,12 +19,11 @@ function mostrarVentanaCookies() {
                 <a href="./index.html"><img src="../img/logoSinFondo.png" alt="Logo de Stellar Us" title="STELLAR US" width="150px" height="150px" style="padding: 30px;"/></a>
                 </td>
                 </tr>
-                <tr>
-                <td>
-                <button id="btnAceptarCookies" style="padding: 15px 35px;">Aceptar</button>
-                </td>
-                </tr>
-            </table>
+               </table>
+            </div>
+            <div>
+            <button id="btnAceptarCookies" style="padding: 15px 35px;">Aceptar</button>
+            </div>
             </div>
     `;
     document.body.appendChild(ventanaEmergente);
